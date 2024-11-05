cafb99220fa22be06f72664ad449399d89bb941c
CherryRadiator <fursayuriy911@gmail.com>
2024-11-05 10:06:51 +0300

    main6.py added

---
+++ main6.py
@@ -0,0 +1,37 @@
+capitalLetters = list("ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ")
+lowercaseLetters = list("йцукенгшщзхъфывапролджэячсмитьбю")
+string = input(str()) + ' '
+print(string)
+stringArray = list(string)
+for i in range(len(stringArray)):
+    if stringArray[i] in capitalLetters:
+        stringArray[i] = lowercaseLetters[capitalLetters.index(stringArray[i])]
+    else: continue
+for i in range(len(stringArray)):
+    print(stringArray[i], end='')
+print('')
+
+newStringArray = []
+lastSpace = 0
+for i in range(len(string)):
+    if string[i] == ' ':
+        newStringArray.append(string[lastSpace:i])
+        lastSpace = i
+    else: continue
+
+maxVowelsWord = ''
+maxVowelsWords = []
+Vowels = list('аоеуэыяию')
+print(Vowels)
+maxVowels = 0
+countVowels = 0
+for i in range(len(newStringArray)):
+    for j in range(len(newStringArray[i])):
+        for k in range(len(newStringArray[i][j])):
+            for n in range(len(Vowels)):
+                if n == k:
+                    countVowels += 1
+            maxVowels = max(maxVowels, countVowels)
+    maxVowelsWords.append(maxVowels)
+    countVowels = 0
+print(newStringArray[maxVowelsWords.index(maxVowels)])
